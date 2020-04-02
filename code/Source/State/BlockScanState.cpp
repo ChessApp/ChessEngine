aec057914821aa21f709e164a0bcaab8e8113c7c
Kevin Scott <kmscott27@gmail.com>
2020-04-02 13:55:37 -0700

    Comment BlockScanState logic

--- code/Source/State/BlockScanState.cpp
+++ code/Source/State/BlockScanState.cpp
@@ -24,24 +24,28 @@ namespace Chess
           PiecePtr threatPiece = currentTurn_->getCausingCheckPiece(i);
           BaseTurn::PieceList & activeList = defender->getActiveList();
           
-          for( BaseTurn::PieceList::iterator p = activeList.begin(); p != activeList.end(); ++p )
+          for( BaseTurn::PieceList::iterator defendingPiece = activeList.begin(); defendingPiece != activeList.end(); ++defendingPiece )
           {
-            if( (*p)->getType() != "K" )
+            // A King cannot block itself. Ignore the king as a potential defending piece.
+            if( (*defendingPiece)->getType() != "K" )
             {
+              // Check if the defending piece can move anywhere on the attack path to block it.
               for( int it = 0; it < (*s)->getRowList().size(); it++ )
               {
                 int row = (*s)->getRowList()[it];
                 int col = (*s)->getColList()[it];
 
-                if( (*p)->validDirection(row, col) && (*p)->pathScan(row, col) && !(*p)->getPinnedStatus() )
+                // Defending piece must be able to move to the square, cannot be blocked, and also cannot be pinned.
+                if( (*defendingPiece)->validDirection(row, col) && (*defendingPiece)->pathScan(row, col) && !(*defendingPiece)->getPinnedStatus() )
                 {
-                  cout << "Type: " << (*p)->getType() << " Color: " << (*p)->getColor() << " DestRow: " << row << " DestCol: " << col << endl;
+                  cout << "Type: " << (*defendingPiece)->getType() << " Color: " << (*defendingPiece)->getColor() << " DestRow: " << row << " DestCol: " << col << endl;
                   return returnState_;
                 }
               }
-              if( (*p)->validDirection(threatPiece->getRow(), threatPiece->getCol()) && (*p)->pathScan(threatPiece->getRow(), threatPiece->getCol()) && !(*p)->getPinnedStatus() )
+              // Check if the defending piece can attack the threat piece.
+              if( (*defendingPiece)->validDirection(threatPiece->getRow(), threatPiece->getCol()) && (*defendingPiece)->pathScan(threatPiece->getRow(), threatPiece->getCol()) && !(*defendingPiece)->getPinnedStatus() )
               {
-                cout << "Type: " << (*p)->getType() << " Color: " << (*p)->getColor() << " can neutralize threat from" << " Row: " << (*p)->getRow() << " Col: " << (*p)->getCol() << endl;
+                cout << "Type: " << (*defendingPiece)->getType() << " Color: " << (*defendingPiece)->getColor() << " can neutralize threat from" << " Row: " << (*defendingPiece)->getRow() << " Col: " << (*defendingPiece)->getCol() << endl;
                 return returnState_;
               }
             }
