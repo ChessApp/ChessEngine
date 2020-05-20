f18ea6f99be207d8833df71ed5584b59c627647a
Kevin Scott <kmscott27@gmail.com>
2020-05-19 23:12:51 -0700

    T#76ekep: GE: Automate writing long list of input moves

---
+++ debugging/ExecuteInputList.py
@@ -0,0 +1,20 @@
+import os.path
+from os import path
+
+UserInputFilePath = "/home/chess/dev/ChessGame/code/Source/UI/www/UserInput.txt"
+UserInputListPath = "/home/chess/dev/ChessGame/build/InputFile.txt"
+
+listFile = open(UserInputListPath)
+lines = listFile.readlines()
+
+for line in lines:
+  # Wait for game engine to consume current input
+  while path.exists(UserInputFilePath) == True:
+    pass
+
+  if path.exists(UserInputFilePath) == False:
+    line = line.strip("\n")
+    print(line)
+    targetFile = open(UserInputFilePath, "w")
+    targetFile.write(line)
+    targetFile.close()
