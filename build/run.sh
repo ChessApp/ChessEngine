52ace2ed869d4d4615e3c0220045da5c499fdc4b
Kevin Scott <kmscott27@gmail.com>
2020-05-19 22:31:25 -0700

    T#6rfkmd: GE: Always store game output to a file. Allow output to tty as well.

--- build/run.sh
+++ build/run.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-./chess-all < InputFile.txt > OutputFile.txt
-
-FILE=OutputFile.txt
-STRING="Checkmate!"
-if [ ! -z $(grep "$STRING" "$FILE") ]; then echo "PASS"; else echo "FAIL"; fi
+./chess-all DefaultInitialState.xml 2>&1 | tee OutputFile.txt
