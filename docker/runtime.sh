d1faaac61d1fcc9be90b726d92138fbb0ff47bc6
kevin <kmscott27@gmail.com>
2021-01-05 20:20:21 -0800

    Updated script to search for aws credentials

--- docker/runtime.sh
+++ docker/runtime.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-docker run --rm -it \
+KEY=$(sed -n 1p ../secrets/aws_credentials.secret)
+SECRET=$(sed -n 2p ../secrets/aws_credentials.secret)
+
+docker run \
+  --rm -it \
+  --network host \
   -e DOCKER_LAMBDA_STAY_OPEN=1 \
+  -e AWS_ACCESS_KEY_ID=$KEY \
+  -e AWS_SECRET_ACCESS_KEY=$SECRET \
   -p 9001:9001 \
   -v $(pwd)/mount:/var/task \
   lambci/lambda:provided \
